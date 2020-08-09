981da4d72cbdf82f2fbc2cdac00de370358602ac
Radu Stefan <radu.stefan@dynatrace.com>
2020-08-09 14:33:41 +0100

    simplified mockserver payloads; updated details as expected by circleCI

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -6,8 +6,8 @@ from tests import tooling_for_test as testtools
 from dynatrace.requests.request_handler import TenantAPIs
 from dynatrace.tenant.topology import process
 
-cluster = FULL_SET.get('mock_cluster')
-tenant = 'mock_tenant'
+cluster = FULL_SET.get('mockcluster1')
+tenant = 'tenant1'
 url = f"{TenantAPIs.V1_TOPOLOGY}/infrastructure/processes"
 request_dir = "tests/mockserver_payloads/requests/processes"
 response_dir = "tests/mockserver_payloads/responses/processes"
@@ -34,7 +34,7 @@ class TestGetProcesses(unittest.TestCase):
     def test_get_single_process(self):
         """Tests getting one specific process."""
         response_file = f"{response_dir}/get_one_pgi.json"
-        process_id = "PROCESS_GROUP_INSTANCE-718687D9E9D0D7CE"
+        process_id = "PROCESS_GROUP_INSTANCE-ABC123DEF456GHI7"
 
         testtools.create_mockserver_expectation(
             cluster=cluster,
