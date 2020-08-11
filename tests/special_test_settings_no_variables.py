5b23c05faa444c528aba0df8db6b4dad1252332f
Aaron <aaronphilipose@gmail.com>
2020-08-11 14:07:32 -0500

    #PAF-46 Linting for FLAKE8

--- tests/special_test_settings_no_variables.py
+++ tests/special_test_settings_no_variables.py
@@ -1,11 +1,13 @@
 import unittest
 from os import rename
 
+
 class TestSettingsWithoutVarFile(unittest.TestCase):
+    """Special test cases when user_variables is absent"""
     def test_settings_without_var_file(self):
+        """Test should return default value when user_variables missing"""
         rename("user_variables.py", "user_variables.py.tmp")
         from dynatrace import settings
         tz = settings.get_setting("DEFAULT_TIMEZONE")
         self.assertEqual(tz, "UTC")
         rename("user_variables.py.tmp", "user_variables.py")
-
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,14 +1,19 @@
 import unittest
 from dynatrace import settings
 
+
 class TestSettings(unittest.TestCase):
     def test_get_setting_from_user_variable(self):
         tz = settings.get_setting("DEFAULT_TIMEZONE")
         self.assertEqual(tz, "America/Chicago")
+
     def test_get_setting_from_default(self):
         log_dir = settings.get_setting("LOG_DIR")
         self.assertEqual(log_dir, "logs/")
+
     def test_get_invalid_setting(self):
         with self.assertRaises(AttributeError) as context:
             settings.get_setting("INVALID_SETTING")
-        self.assertIn("not a valid user variable attribute", str(context.exception))
+
+        self.assertIn("not a valid user variable attribute",
+                      str(context.exception))
