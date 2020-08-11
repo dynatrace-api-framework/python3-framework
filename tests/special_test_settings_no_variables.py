f979e4a65f3f9211835cb00df3ece0d990f3000a
Aaron <aaronphilipose@gmail.com>
2020-08-11 15:52:52 -0500

    PAF-46 #Updating pylintrc [skip ci]

--- tests/special_test_settings_no_variables.py
+++ tests/special_test_settings_no_variables.py
@@ -1,3 +1,4 @@
+"""Special Cases for settings which requires user_variables.py to NOT exist"""
 import unittest
 from os import rename
 
@@ -7,7 +8,7 @@ class TestSettingsWithoutVarFile(unittest.TestCase):
     def test_settings_without_var_file(self):
         """Test should return default value when user_variables missing"""
         rename("user_variables.py", "user_variables.py.tmp")
-        from dynatrace import settings
-        tz = settings.get_setting("DEFAULT_TIMEZONE")
-        self.assertEqual(tz, "UTC")
+        from dynatrace import settings # pylint: disable=import-outside-toplevel
+        timezone = settings.get_setting("DEFAULT_TIMEZONE")
+        self.assertEqual(timezone, "UTC")
         rename("user_variables.py.tmp", "user_variables.py")
