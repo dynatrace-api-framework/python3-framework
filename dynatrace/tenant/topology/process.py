da2b014aad7d325594a1cefb0ada0b8b9877070e
Radu Stefan <radu.stefan@dynatrace.com>
2020-10-08 13:23:25 +0100

    updated topology.processes to use the new topology.shared functions

--- dynatrace/tenant/topology/process.py
+++ dynatrace/tenant/topology/process.py
@@ -1,12 +1,21 @@
-"""Process operations from the Dynatrace API"""
-import dynatrace.tenant.topology.shared as topology_shared
+import dynatrace.tenant.topology.shared as entity_api
 
 
 def get_processes_tenantwide(cluster, tenant, params=None):
     """Get Information for all processes in a tenant"""
-    return topology_shared.get_env_layer_entities(cluster, tenant, 'processes', params=params)
+    return entity_api.get_entities(
+        cluster=cluster,
+        tenant=tenant,
+        entity_type=entity_api.EntityTypes.PROCESS_GROUP_INSTANCE,
+        params=params
+    )
 
 
 def get_process(cluster, tenant, entity, params=None):
     """Get Information on one process for in a tenant"""
-    return topology_shared.get_env_layer_entity(cluster, tenant, 'processes', entity, params=params)
+    return entity_api.get_entity(
+        cluster=cluster,
+        tenant=tenant,
+        entity_id=entity,
+        params=params
+    )
