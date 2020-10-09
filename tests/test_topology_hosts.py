ccee844037e89b64a8a27efaf1ba006595266dcc
Radu Stefan <radu.stefan@dynatrace.com>
2020-10-09 06:36:50 +0100

    updated hosts test suite

--- tests/test_topology_hosts.py
+++ tests/test_topology_hosts.py
@@ -2,14 +2,16 @@
 Test Suite for Topology Hosts
 """
 import unittest
-from user_variables import FULL_SET  # pylint: disable=import-error
+from variable_sets.radu_vars import FULL_SET  # pylint: disable=import-error
 from tests import tooling_for_test as testtools
 from dynatrace.requests.request_handler import TenantAPIs
 from dynatrace.tenant.topology import hosts
 
 CLUSTER = FULL_SET["mockserver1"]
 TENANT = "tenant1"
-URL_PATH = f"{TenantAPIs.V1_TOPOLOGY}/infrastructure/hosts"
+URL_PATH = str(TenantAPIs.ENTITIES)
+V1_URL_PATH = f'{TenantAPIs.V1_TOPOLOGY}/infrastructure/hosts'
+TAG_URL_PATH = str(TenantAPIs.TAGS)
 REQUEST_DIR = "tests/mockserver_payloads/requests/hosts"
 RESPONSE_DIR = "tests/mockserver_payloads/responses/hosts"
 
@@ -27,28 +29,36 @@ class TestGetHosts(unittest.TestCase):
             tenant=TENANT,
             url_path=URL_PATH,
             request_type="GET",
+            parameters={
+                'entitySelector': 'type("HOST")'
+            },
             response_file=response_file
         )
 
         result = hosts.get_hosts_tenantwide(CLUSTER, TENANT)
-        self.assertEqual(result, testtools.expected_payload(response_file))
+        expected_result = testtools.expected_payload(response_file).get('entities')
+        self.assertEqual(result, expected_result)
 
     def test_get_single_host(self):
         """Test fetching a specific host"""
 
-        host_id = "HOST-9F74450267BAAE20"
+        host_id = "HOST-ABC123DEF456GHIJ"
         response_file = f"{RESPONSE_DIR}/get_single.json"
 
         testtools.create_mockserver_expectation(
             cluster=CLUSTER,
             tenant=TENANT,
-            url_path=f"{URL_PATH}/{host_id}",
+            url_path=URL_PATH,
             request_type="GET",
+            parameters={
+                'entitySelector': f'entityId({host_id})'
+            },
             response_file=response_file
         )
 
         result = hosts.get_host(CLUSTER, TENANT, host_id)
-        self.assertEqual(result, testtools.expected_payload(response_file))
+        expected_result = testtools.expected_payload(response_file).get('entities')[0]
+        self.assertEqual(result, expected_result)
 
     def test_get_host_count(self):
         """Test getting the count of hosts in a tenant."""
@@ -60,8 +70,10 @@ class TestGetHosts(unittest.TestCase):
             url_path=URL_PATH,
             request_type="GET",
             response_file=response_file,
-            parameters=dict(relativeTime=['day'],
-                            includeDetails=['False'])
+            parameters={
+                'from': 'now-24h',
+                'entitySelector': 'type("HOST")'
+            }
         )
 
         result = hosts.get_host_count_tenantwide(CLUSTER, TENANT)
@@ -70,11 +82,11 @@ class TestGetHosts(unittest.TestCase):
     def test_get_host_units(self):
         """Tests getting the consumed host units in a tenant."""
 
-        response_file = f"{RESPONSE_DIR}/get_all.json"
+        response_file = f"{RESPONSE_DIR}/v1_get_all.json"
         testtools.create_mockserver_expectation(
             cluster=CLUSTER,
             tenant=TENANT,
-            url_path=URL_PATH,
+            url_path=V1_URL_PATH,
             request_type="GET",
             response_file=response_file
         )
@@ -97,13 +109,16 @@ class TestHostTagging(unittest.TestCase):
             cluster=CLUSTER,
             tenant=TENANT,
             request_type="POST",
-            url_path=f"{URL_PATH}/{host_id}",
+            url_path=TAG_URL_PATH,
             request_file=request_file,
+            parameters={
+                'entitySelector': f'entityId({host_id})'
+            },
             response_code=201
         )
 
         result = hosts.add_host_tags(CLUSTER, TENANT, host_id, tags)
-        self.assertEqual(result, 201)
+        self.assertEqual(result.status_code, 201)
 
     def test_delete_tags(self):
         """Test deleting a tag from a specific host."""
@@ -114,7 +129,7 @@ class TestHostTagging(unittest.TestCase):
         testtools.create_mockserver_expectation(
             cluster=CLUSTER,
             tenant=TENANT,
-            url_path=f"{URL_PATH}/{host_id}/tags/{tag}",
+            url_path=TAG_URL_PATH,
             request_type="DELETE",
             response_code=204
         )
