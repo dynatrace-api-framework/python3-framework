ac4468ccb7b8374880185522253798f2c550078e
Radu Stefan <radu.stefan@dynatrace.com>
2020-12-04 08:23:42 +0000

    PAF-67: missing test cases for problems.py

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -1,6 +1,6 @@
 """Test Suite for the Extensions API"""
 import unittest
-from user_variables import FULL_SET  # pylint: disable=import-error
+from variable_sets.radu_vars import FULL_SET  # pylint: disable=import-error
 from tests import tooling_for_test as testtools
 from dynatrace.framework.request_handler import TenantAPIs, HTTP
 from dynatrace.tenant import problems
@@ -64,8 +64,9 @@ class TestGetProblems(unittest.TestCase):
         )
 
         result = problems.get_problem_count(CLUSTER, TENANT)
+        expected_result = 2
 
-        self.assertEqual(result, 2)
+        self.assertEqual(result, expected_result)
 
     def test_get_all_comments(self):
         """Tests fetching all comments of a problem"""
@@ -115,9 +116,12 @@ class TestModifyProblems(unittest.TestCase):
             request_type=str(HTTP.POST)
         )
 
-        result = problems.close_problem(CLUSTER, TENANT, PROBLEM_ID)
+        result = problems.close_problem(
+            CLUSTER, TENANT, PROBLEM_ID
+        ).status_code
+        expected_result = 204
 
-        self.assertEqual(result.status_code, 204)
+        self.assertEqual(result, expected_result)
 
     def test_add_comment(self):
         """Tests adding a comment to a problem"""
@@ -128,7 +132,7 @@ class TestModifyProblems(unittest.TestCase):
             tenant=TENANT,
             url_path=f"{URL_PATH}/{PROBLEM_ID}/comments",
             request_file=request_file,
-            response_code=200,
+            response_code=201,
             request_type=str(HTTP.POST)
         )
 
@@ -136,37 +140,37 @@ class TestModifyProblems(unittest.TestCase):
             cluster=CLUSTER,
             tenant=TENANT,
             problem_id=PROBLEM_ID,
-            comment="Test comment",
+            message="Test comment",
             context="Test"
-        )
+        ). status_code
+        expected_result = 201
 
-        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result, expected_result)
 
-    def test_update_comment(self):
-        """Tests updating an existing comment of problem"""
-        response_file = f"{RESPONSE_DIR}/get_comment.json"
+    def test_update_comment_with_data(self):
+        """Tests updating an existing comment of problem.
+        Comment and context provided.
+        """
+        request_file = f"{REQUEST_DIR}/updated_comment.json"
+        message = "updated comment"
+        context = "test_context"
 
         testtools.create_mockserver_expectation(
             cluster=CLUSTER,
             tenant=TENANT,
             url_path=f"{URL_PATH}/{PROBLEM_ID}/comments/{COMMENT_ID}",
-            request_type=str(HTTP.GET),
-            response_file=response_file,
-            mock_id="call1"
-        )
-        testtools.create_mockserver_expectation(
-            cluster=CLUSTER,
-            tenant=TENANT,
-            url_path=f"{URL_PATH}/{PROBLEM_ID}/comments/{COMMENT_ID}",
-            request_file=response_file,
+            request_file=request_file,
             response_code=204,
-            request_type=str(HTTP.PUT),
-            mock_id="call2"
+            request_type=str(HTTP.PUT)
         )
 
-        result = problems.update_comment(CLUSTER, TENANT, PROBLEM_ID, COMMENT_ID)
+        result = problems.update_comment(
+            CLUSTER, TENANT, PROBLEM_ID, COMMENT_ID,
+            message=message, context=context
+        ).status_code
+        expected_result = 204
 
-        self.assertEqual(result.status_code, 204)
+        self.assertEqual(result, expected_result)
 
     def test_delete_comment(self):
         """Tests deleting a comment from a problem"""
@@ -178,9 +182,12 @@ class TestModifyProblems(unittest.TestCase):
             response_code=204
         )
 
-        result = problems.delete_comment(CLUSTER, TENANT, PROBLEM_ID, COMMENT_ID)
+        result = problems.delete_comment(
+            CLUSTER, TENANT, PROBLEM_ID, COMMENT_ID
+        ).status_code
+        expected_result = 204
 
-        self.assertEqual(result.status_code, 204)
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == "__main__":
