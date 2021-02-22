0f462792b1d57b755050b30f75a2dbb2960fb848
Aaron <aaronphilipose@gmail.com>
2021-02-22 00:09:11 -0500

    PAF-84 #Linting Cleanup Recommended

--- scripts/template.py
+++ scripts/template.py
@@ -1,4 +1,5 @@
+"""Template to make your own scripts"""
 # IMPORTS GO HERE
-import change_pythonpath # Must be first import
+import change_pythonpath # Must be first import pylint: disable=unused-import
 
 # YOUR SCRIPT GOES HERE
