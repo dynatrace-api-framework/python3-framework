dd2362064291f028d19966f13b2383f648f25566
Radu <radu.stefan@dynatrace.com>
2021-04-09 18:45:18 +0100

    modified path of test

--- tests/test_request_handler.py
+++ tests/test_request_handler.py
@@ -88,7 +88,7 @@ class TestRequests(unittest.TestCase):
         testools.create_mockserver_expectation(
             cluster=MGD_CLUSTER,
             tenant=TENANT,
-            url_path=f"/e/{MGD_CLUSTER['tenant'][TENANT]}/testpath",
+            url_path="/testpath",
             request_type=str(rh.HTTP.GET),
             response_code=200,
             mock_id="managed"
